6366ba8a808978e2a4ebe02916b36278834d688f
lbryant-sss <brianmungai350@gmail.com>
2024-08-03 01:01:15 +0300

    Mobile UI update

--- job-search-react-app/src/pages/Landingpage.tsx
+++ job-search-react-app/src/pages/Landingpage.tsx
@@ -1,6 +1,6 @@
 import Menubar from "../assets/menubar.svg";
 import Google from "../assets/Google__G__logo.svg";
-
+import Prof1 from "../assets/prof1.png";
 export default function Landingpage() {
     return(
         <>
@@ -9,9 +9,22 @@ export default function Landingpage() {
                     <span>
                         <img src={Menubar} alt="Menu Bar Icon" />
                     </span>
-                    <h1>
-                        Find your dream job
-                    </h1>
+                    <div className="nav">
+                        <ul>
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="hero">
+                        <img src={Prof1} />
+                        <h1>
+                            Find your dream job
+                        </h1>
+                    </div>
                 </div>
                 <div className="hero-wrapper">
                     <div className="horizontal-scroll">
@@ -20,12 +33,20 @@ export default function Landingpage() {
                             <li>Category</li>
                             <li>Category</li>
                             <li>Category</li>
-                            <li>Category</li>
-                            <li>Category</li>
-                            <li>Category</li>
                         </ul>
                     </div>
                     <div className="card-wrapper">
+                        <div className="job-card">
+                            <div className="company">
+                                <img src={Google} alt="Company Logo" />
+                                <span className="company-name">Google</span>
+                                <span className="date-posted">Posted: 2 weeks ago</span>
+                            </div>
+                            <div className="job-tile">
+                                <span className="job-title">Software Engineering</span>
+                                <span className="apply">Apply</span>
+                            </div>
+                        </div>
                         <div className="job-card">
                             <div className="company">
                                 <img src={Google} alt="Company Logo" />
